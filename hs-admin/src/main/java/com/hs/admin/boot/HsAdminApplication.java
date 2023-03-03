e135204e97754c8a953be03aa6667bbed7a88a46
hou <460713171@qq.com>
2023-03-03 21:16:08 +0800

    readme 更新

--- hs-admin/src/main/java/com/hs/admin/boot/HsAdminApplication.java
+++ hs-admin/src/main/java/com/hs/admin/boot/HsAdminApplication.java
@@ -1,21 +1,20 @@
-package com.hs.hswebapp;
+package com.hs.admin.boot;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@ComponentScan("com.hs.*")
+@ComponentScan("com.hs.admin.*")
 @MapperScan("com.hs.common.**")
 @SpringBootApplication
 @Slf4j
-public class HsWebappApplication {
+public class HsAdminApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(HsWebappApplication.class, args);
+        SpringApplication.run(HsAdminApplication.class, args);
     }
 
 }
